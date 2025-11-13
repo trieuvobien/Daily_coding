e59a6683e316ac35664e6db2918c6e7a766b9aed
trieuvobien <hosyhuy29@gmail.com>
2025-11-13 21:25:41 +0700

    thay doi ngay 14-11

---
+++ ngay 14-11/bla_bla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("hello world");
+    printf("hello world2");
+    printf("thay");
+}
+
+int tinhtong(int a, int b) {
+    return a + b;
+}
+
+int tinhhieu(int a, int b) {
+    return a - b;
+}
+
