f9121194028d60fcabf2b45308b824b6a052b938
trieuvobien <hosyhuy29@gmail.com>
2025-11-13 21:19:58 +0700

    tinhtongtinhhieu

--- ngay13-11/main.c
+++ ngay13-11/main.c
@@ -2,4 +2,13 @@
 
 int main() {
     printf("hello world");
+    printf("hello world2");
+}
+
+int tinhtong(int a, int b) {
+    return a + b;
+}
+
+int tinhhieu(int a, int b) {
+    return a - b;
 }
