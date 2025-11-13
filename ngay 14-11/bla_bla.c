40bda98f7f483115b4cd1faf0c3013349fac45ce
trieuvobien <hosyhuy29@gmail.com>
2025-11-13 21:34:02 +0700

    bla bla

--- ngay 14-11/bla_bla.c
+++ ngay 14-11/bla_bla.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
-
-int main() {
-    printf("hello world");
-    printf("hello world2");
-    printf("thay");
+#include <stdlib.h>
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                int* result = (int*)malloc(2 * sizeof(int));
+                if (result == NULL) {
+                    *returnSize = 0;
+                    return NULL;
+                }
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
 }
-
-int tinhtong(int a, int b) {
-    return a + b;
-}
-
-int tinhhieu(int a, int b) {
-    return a - b;
-}
-
